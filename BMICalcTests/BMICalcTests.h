1366fa65a2f3bac40ca97d9e33d4b19f44046ac7
mattari <mattari.world@gmail.com>
2012-05-14 01:43:58 +0900

    initial commit
    initial commit

---
+++ BMICalcTests/BMICalcTests.h
@@ -0,0 +1,13 @@
+//
+//  BMICalcTests.h
+//  BMICalcTests
+//
+//  Created by 毅好 小板橋 on 12/05/13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BMICalcTests : SenTestCase
+
+@end
---
+++ BMICalcTests/BMICalcTests.m
@@ -0,0 +1,32 @@
+//
+//  BMICalcTests.m
+//  BMICalcTests
+//
+//  Created by 毅好 小板橋 on 12/05/13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "BMICalcTests.h"
+
+@implementation BMICalcTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BMICalcTests");
+}
+
+@end
